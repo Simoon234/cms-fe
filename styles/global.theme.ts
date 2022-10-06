f18f942b31708c184a54a94da0adf3f1e17cf99d
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-06 15:23:31 +0200

    Installed all necessary packages for styling components.

--- styles/global.theme.ts
+++ styles/global.theme.ts
@@ -10,14 +10,16 @@ export interface TypeTheme {
     }
 }
 
-export const theme: TypeTheme = {
+const theme: TypeTheme = {
     colors: {
         mainFontColor: 'white',
         backgroundColor: 'rgba(62, 70, 84, 1)',
-        logoBg: '#4B5563'
+        logoBg: '#4B5563',
     },
     font: {
         fontFamily: 'Patrick Hand, cursive;',
-        h3FontColor: '#FFFFFF'
-    }
+        h3FontColor: '#FFFFFF',
+    },
 }
+
+export default theme
