42a4025efb9fb37160f50f97be40fa6e222779ae
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-06 12:42:10 +0200

    Added <Layout/> to index /

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
 import type { NextPage } from 'next'
 import styled from 'styled-components'
-import Head from 'next/head'
+import { Layout } from '../layout/Layout'
 
 const Home: NextPage = () => {
     return (
         <>
-            <Head>
-                <title>Home App</title>
-                <link rel='preconnect' href='https://fonts.googleapis.com' />
-                <link rel='preconnect' href='https://fonts.gstatic.com' />
-                <link href='https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&display=swap'
-                      rel='stylesheet' />
-            </Head>
-            <div>
-                <Text>siema</Text>
-            </div>
+            <Layout title='Blog for you' keywords='sport, luxury, fashion' description='Super modern blog content'>
+                <div>
+                    <h1>Test</h1>
+                </div>
+            </Layout>
         </>
 
     )
 }
 
 export const Text = styled.p`
-  color: ${props => props.theme.color}
+  color: ${(props) => props.theme.colors.mainFontColor}
 `
 
 
