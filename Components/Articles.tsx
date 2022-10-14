8e5b4917407087eb3f05d0e70881529259ba64c3
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-14 17:27:07 +0200

    smooth scroll after click - pagination

--- Components/Articles.tsx
+++ Components/Articles.tsx
@@ -16,7 +16,7 @@ const Articles = ({data, isLoading, totalPages, page, handlePage}: Articles) => 
     const firstLetterUppercase = (text: string) => text.charAt(0).toUpperCase() + text.slice(1)
     return (
         <ArticlesSection>
-            <h1 className='header'>Articles</h1>
+            <h1 id='articles' className='header'>Articles</h1>
             <ArticlesWrapper>
                 {data.data && data.data.map((item: ResponseDataType) =>
                     <>
@@ -24,15 +24,13 @@ const Articles = ({data, isLoading, totalPages, page, handlePage}: Articles) => 
                             description={item.attributes.description.length < 300 ? item.attributes.description : `${item.attributes.description.slice(0, 166)}...`}
                             key={item.id}
                             id={item.id.toString()}
-                            avatar={`http://localhost:1337`}
+                            avatar={item.attributes.author.data.attributes.avatar}
                             category={item.attributes.categories}
                             job={item.attributes.author.data.attributes.job}
-                            person={`${firstLetterUppercase(item.attributes.author.data.attributes.name)} ${item.attributes.author.data.attributes.surname.slice(0, 2)}.`}
+                            person={`${firstLetterUppercase(item.attributes.author.data.attributes.firstname)} ${item.attributes.author.data.attributes.lastname.slice(0, 2)}.`}
                             date={new Date(item.attributes.createdAt).toLocaleString()}
                             title={item.attributes.title}
                             src={`http://localhost:1337${item.attributes.photo.data.map((item) => item.attributes.formats.thumbnail.url)}`}
-                            height={item.attributes.photo.data.map((item) => item.attributes.height)[0]}
-                            width={item.attributes.photo.data.map((item) => item.attributes.width)[0]}
                         />}
                     </>,
                 )}
