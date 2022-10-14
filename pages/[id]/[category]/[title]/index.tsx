e7262f82de514d602e5787fdce83464d814daea1
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-14 17:03:36 +0200

    fixing bug

--- pages/[id]/[category]/[title]/index.tsx
+++ pages/[id]/[category]/[title]/index.tsx
@@ -2,24 +2,24 @@ import styled from 'styled-components'
 import Categories from '../../../../Components/common/Categories'
 import Author from '../../../../Components/common/Author'
 import Image from 'next/image'
-import { GetStaticPaths, GetStaticProps } from 'next'
-import { ResponseDataType } from '../../../../types'
+import {GetStaticPaths, GetStaticProps} from 'next'
+import {ResponseDataType} from '../../../../types'
+import {DEFAULT_URL} from "../../../../api.config";
 
 interface Article {
     obj: {
         article: ResponseDataType;
-        author: any
     }
 }
 
-const SingleArticleInfoDetails = ({ obj }: Article) => {
-    const photo = obj.article.attributes.photo.data.map(item => item.attributes)[0]
-
-    console.log(obj.article.attributes.categories)
+const SingleArticleInfoDetails = ({obj}: Article) => {
+    const photo = obj?.article?.attributes?.photo?.data?.map(item => item?.attributes)[0];
+    console.log(obj)
+    const avatar = obj?.article?.attributes?.author?.data?.attributes?.avatar;
     return (
         <Details>
             <Categories category={obj?.article?.attributes?.categories}
-                        date={new Date(obj?.article?.attributes?.createdAt).toLocaleString()} />
+                        date={new Date(obj?.article?.attributes?.createdAt).toLocaleString()}/>
             <div className='content'>
                 <h3>
                     {obj?.article?.attributes?.title}
@@ -27,12 +27,14 @@ const SingleArticleInfoDetails = ({ obj }: Article) => {
             </div>
             <div className='author'>
                 <Author
-                    src={`http://localhost:1337${obj?.author?.attributes?.avatar?.data?.attributes?.formats?.thumbnail?.url}`}
+                    src={avatar}
                     job={obj?.article?.attributes?.author?.data?.attributes?.job}
-                    person={obj?.article?.attributes?.author?.data?.attributes?.name} />
+                    person={obj?.article?.attributes?.author?.data?.attributes?.firstname}/>
             </div>
-            <Image src={`http://localhost:1337${photo?.formats?.thumbnail?.url}`} width={photo?.width}
-                   height={photo?.height} />
+            <div>
+                <Image alt='big image' src={`${DEFAULT_URL}${photo?.formats?.thumbnail?.url}`}
+                       width={photo?.formats?.large?.width || 1000} height={photo?.formats?.large?.width || 600}/>
+            </div>
             <div className='description'>
                 <p>{obj?.article?.attributes?.description}</p>
             </div>
@@ -92,11 +94,12 @@ export const Details = styled.div`
 export const getStaticPaths: GetStaticPaths = async () => {
     const res = await fetch(`http://localhost:1337/api/articles?populate=author,photo`)
     const data = await res.json()
+    const illegal = /[&\/\\#,+()$~%.'":*?<>{}]/g;
     const paths = data.data.map((item: ResponseDataType) => ({
         params: {
             id: item.id.toString(),
             category: item.attributes.categories,
-            title: item.attributes.title,
+            title: item.attributes.title.replace(illegal, ''),
         },
     }))
     return {
@@ -110,15 +113,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
     const data = await res.json()
     const article = data.data.find((item: ResponseDataType) => (item.id.toString()) === context.params?.id)
 
-    const user = await fetch(`http://localhost:1337/api/authors?populate=avatar`)
-    const userData = await user.json()
-
-
-    const author = userData.data.find((user: any) => user.id === article.attributes.author.data.id)
-
     const obj = {
         article,
-        author,
     }
 
     return {
