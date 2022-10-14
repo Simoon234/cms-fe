d3aa89e65f7d75722b9e02777e099a713a824b72
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-14 17:24:50 +0200

    get all articles by user id

--- pages/article/all/index.tsx
+++ pages/article/all/index.tsx
@@ -1,16 +1,23 @@
 import {GetServerSideProps} from 'next'
 import {Box} from '../../../Components/common/Box'
-
 import {API_URL} from '../../../api.config'
 import {ResponseDataType} from '../../../types'
+import {UseAppSelectorHook} from '../../../hooks/useAppSelectorHook'
 
 
 const AllArticles = (data: any) => {
+    const {id, isAuthMessage} = UseAppSelectorHook(state => state.user);
+    console.log(id)
+    const getAllArticlesByAuthor = data.data.filter((item: any) => {
+        return item.attributes.author.data.id === id
+    })
+    console.log(getAllArticlesByAuthor)
     return (
         <>
-            {data.data && data.data.map((item: ResponseDataType) =>
+            {(isAuthMessage === '' && id) ? getAllArticlesByAuthor && getAllArticlesByAuthor.map((item: ResponseDataType) =>
                 <Box key={item.id} attributes={item.attributes} id={item.id}/>,
-            )}
+            ) : <h1>{isAuthMessage}</h1>}
+
         </>
     )
 }
