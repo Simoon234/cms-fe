002b92d38ec5f41a2b4ce579774ede1071aa8207
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-14 17:07:44 +0200

    types, needed for better typing.

--- types/ts/index.ts
+++ types/ts/index.ts
@@ -16,9 +16,27 @@ export interface Image {
         width: number;
         height: number;
         formats: {
-            thumbnail: {
+            large?: {
                 mime: string;
                 url: string;
+                height: number;
+                width: string;
+            }
+            small?: {
+                mime: string;
+                url: string;
+                height: number;
+                width: string;
+            }
+            medium?: {
+                mime: string;
+                url: string;
+                height: number;
+                width: string;
+            }
+            thumbnail: {
+                url: string;
+                width: string;
             }
         }
     }
@@ -28,9 +46,10 @@ export interface Author {
     data: {
         id: string;
         attributes: {
-            name: string;
+            firstname: string;
             job: string;
-            surname: string;
+            lastname: string;
+            avatar: string;
         }
     }
 }
@@ -49,4 +68,42 @@ export interface ResponseDataType {
         photo: Photo;
         author: Author;
     }
+}
+
+export interface RegisterUser {
+    username: string;
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    avatar: string;
+}
+
+export interface LogUserResponse {
+    identifier: string;
+    password: string;
+}
+
+export interface LogUserResponseData {
+    email: string;
+    password: string;
+}
+
+export interface UserStrapiResponse {
+    jwt: string;
+    user: {
+        blocked: boolean;
+        confirmed: boolean;
+        created_at: string;
+        email: string;
+        id: number;
+        role: {
+            description: string;
+            id: number;
+            name: string;
+            type: string;
+        };
+        updated_at: string;
+        username: string;
+    };
 }
