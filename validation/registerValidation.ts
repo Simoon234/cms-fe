fe276d7aafe7910c1b9d9e2f5b2398f284087b6f
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-14 17:07:13 +0200

    Validation for forms to register and login.

--- validation/registerValidation.ts
+++ validation/registerValidation.ts
@@ -1,27 +1,33 @@
 export interface InitialFormValues {
+    username: string;
     firstname: string,
     lastname: string,
     email: string,
     password: string,
     confirmPassword: string,
+    avatar: string;
 }
 
-export const initialValues = {
+export const initialValues: InitialFormValues = {
+    username: "",
     firstname: "",
     lastname: "",
     email: "",
     password: "",
     confirmPassword: "",
+    avatar: ''
 }
 
-export const validateFunction = () => {
-    const {confirmPassword, password, email, firstname, lastname} = initialValues;
+export const validateRegisterFunction = (values: InitialFormValues) => {
+    const {confirmPassword, password, email, firstname, lastname, username} = values;
     const errors: InitialFormValues = {
+        username: '',
         confirmPassword: '',
         email: '',
         firstname: '',
         lastname: '',
-        password: ''
+        password: '',
+        avatar: ''
     };
     if (!firstname) {
         errors.firstname = 'Required';
@@ -45,6 +51,12 @@ export const validateFunction = () => {
         errors.password = 'Required';
     } else if (!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/.test(password)) {
         errors.password = 'Invalid password (Minimum six characters, at least one uppercase letter, one lowercase letter and one number)';
+    }
+
+    if (!username) {
+        errors.username = 'Required';
+    } else if (username.length < 5) {
+        errors.username = 'Username must be at least 5 characters'
     }
 
     if (!confirmPassword) {
--- validation/schemaValidation.ts
+++ validation/schemaValidation.ts
@@ -9,7 +9,19 @@ export const SignupSchema = Yup.object().shape({
         .min(2, 'Too Short!')
         .max(50, 'Too Long!')
         .required('Required'),
+    username: Yup.string()
+        .min(2, 'Too Short!')
+        .max(15, 'Too Long!')
+        .required('Required'),
     email: Yup.string().email('Invalid email').required('Required'),
     password: Yup.string().min(6).max(30).required('Required'),
     confirmPassword: Yup.string().oneOf([Yup.ref('password'), null], 'Password must match')
+})
+
+
+export const LogSchema = Yup.object().shape({
+    email: Yup.string()
+        .required('Email required'),
+    password: Yup.string()
+        .required('Password required')
 })
