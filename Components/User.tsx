45a4eb5aaebd4667a5a678569f5005f446993dcb
Simoon234 <83337792+Simoon234@users.noreply.github.com>
2022-10-17 10:53:29 +0200

    I have to add isLogged to dependancy array, becaouse I didn't received user from  token stored in LS

--- Components/User.tsx
+++ Components/User.tsx
@@ -1,17 +1,19 @@
 import {UseDispatchHook} from "../hooks/useDispatchHook";
 import {useEffect} from "react";
 import {fetchUser} from "../redux/userSlice";
+import {UseAppSelectorHook} from "../hooks/useAppSelectorHook";
 
 
 export const User = () => {
     const dispatch = UseDispatchHook();
+    const {isSuccessLogin} = UseAppSelectorHook(state => state.user);
     useEffect(() => {
         (async () => {
-            if (localStorage.getItem('token') && localStorage.getItem('token') !== null) {
+            if (localStorage.getItem('token') !== null || localStorage.getItem('token') !== '') {
                 dispatch(await fetchUser(localStorage.getItem('token')?.toString() as string));
             }
         })()
-    }, [dispatch]);
+    }, [dispatch, isSuccessLogin]);
     return (
         <>
         </>
